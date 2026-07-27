e4caa9ed5990b6a0dcc28eaa7f772f83de7596ed
Paridhi Gupta <4paridhi@gmail.com>
2026-07-27 18:56:39 +0000

    --tempearture-model

---
+++ models/temperature_model.sql
@@ -0,0 +1,8 @@
+{% set temperature = 70.0 %}
+
+SELECT 
+    'On a day like this,I especially like ' ||
+    CASE
+        WHEN {{ temperature }} > 70.0 THEN 'a refreshing mango sorbet.'
+        ELSE 'a decadent chocolate ice cream.'
+    END AS message
