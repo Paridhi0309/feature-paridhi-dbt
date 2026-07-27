51cb2b58e4b91c474d01d4bbb5b8b7435064a88e
Paridhi Gupta <4paridhi@gmail.com>
2026-07-27 19:58:34 +0000

    --flavors_model changes

--- models/flavors_model.sql
+++ models/flavors_model.sql
@@ -1,7 +1,7 @@
 {% set flavors=['chocolate','vanilla','strawberry' ]%}
 
 SELECT 'Today I want chocolate ice cream!' AS message
-{% for flavor in flavors[1:] %}
+{% for flavor in flavors[0:] %}
 UNION ALL
 SELECT 'Today I want {{ flavor }} ice cream!' AS message
 {% endfor %}
