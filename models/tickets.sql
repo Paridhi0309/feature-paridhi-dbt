1660b2d5ea3b9f8ea096a29434fd3e2717095a78
Paridhi Gupta <4paridhi@gmail.com>
2026-08-01 18:21:25 +0000

    --snapshots

--- models/tickets.sql
+++ models/tickets.sql
@@ -2,6 +2,4 @@ select 1 as ticket_id,'success' as ticket_status,'2024-12-01' as updated_date
 union ALL
 select 2 as ticket_id,'new' as ticket_status,'2024-12-02' as updated_date
 union ALL
-select 3 as ticket_id,'in-progress' as ticket_status,'2024-12-03' as updated_date
-union ALL
-select 4 as ticket_id,'in-progress' as ticket_status,'2024-12-04' as updated_date 
+select 4 as ticket_id,'failed' as ticket_status,'2024-12-04' as updated_date 
